4cffe95621e7fad36445e600407f685fd4a5d7c1
Ryan Desfosses <ryan@desfo.org>
2016-10-05 17:02:09 -0400

    replaced gulp-minify-css with gulp-clean-css
    in response to:
      npm WARN deprecated gulp-minify-css@1.2.4: Please use gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
-var minifyCSS = require('gulp-minify-css');
+var minifyCSS = require('gulp-clean-css');
  
 gulp.task('sass', function () {
   return gulp.src('codeformaine/static/scss/**/*.scss')
